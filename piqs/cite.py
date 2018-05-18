6326ccbedf93cbf2538d8bb40f7870879ebc31d8
Shahnawaz Ahmed <shahnawaz.ahmed95@gmail.com>
2018-05-18 12:25:37 +0900

    Update cite.py

--- piqs/cite.py
+++ piqs/cite.py
@@ -15,7 +15,14 @@ def cite(path=None, verbose=True):
         The complete directory path to generate the bibtex file.
         If not specified then the citation will be generated in cwd
     """
-    citation = []
+    citation = ["""@misc{1805.05129,""",
+               """Author = {Nathan Shammah and Shahnawaz Ahmed and Neill Lambert
+               and Simone De Liberato and Franco Nori},""",
+               """Title = {Open quantum systems with local and collective incoherent
+               processes: Efficient numerical simulation using permutational invariance},""",
+               """Year = {2018},""",
+               """Eprint = {arXiv:1805.05129},""",
+               """}"""]
 
     if verbose:
         print("\n".join(citation))
@@ -23,7 +30,7 @@ def cite(path=None, verbose=True):
     if not path:
         path = os.getcwd()
 
-    filename = "qutip.bib"
+    filename = "piqs.bib"
     with open(os.path.join(path, filename), 'w') as f:
         f.write("\n".join(citation))
 
