ed48c60e28372886033538a546d65e95753abea2
Shahnawaz Ahmed <shahnawaz.ahmed95@gmail.com>
2018-05-23 17:11:17 +0900

    added ..

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@ import sys
 from unittest.mock import MagicMock, Mock
 
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('..'))
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
